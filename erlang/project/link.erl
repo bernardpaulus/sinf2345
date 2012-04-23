f13522e6ba681380f711c06728ae6618c6460a07
Bernard Paulus <bprecyclebin@gmail.com>
2012-04-23 17:25:22 +0200

    damn_simple_link finished but not debugged

--- erlang/project/link.erl
+++ erlang/project/link.erl
@@ -12,16 +12,36 @@ low_level_link(Node1, Proc_Name1, Pid_Up1, Node2, Proc_Name2, Pid_Up2) ->
 low_level_link_end_loop(Proc_Name, Other_Node, Other_Proc_Name) ->
     
 
+% creates a damn_simple_link between two nodes (they can be equals)
+% @post: the current process has a name
+% @return: [{Name1, Erl_Node1}, {Name2, Erl_Node2}]
 damn_simple_link(Erl_Node1, Erl_Node2) ->
-    % register unique random name
-    % spawn at node fun() -> register_unique_random_name + send random name & wait for
-                % other name
+    Self_name = register_unique(damn_simple_link_spawner, self()),
     % receive both names + exchange those names.
+    spawn(Erl_Node1, link, damn_simple_link_startup, [{Self_name, node()}]),
+    receive {name, Proc1} ->
+        spawn(Erl_Node2, link, damn_simple_link_startup, [{Self_name, node()}]),
+        receive {name, Proc2} ->
+            Proc1 ! {other, Proc2},
+            Proc2 ! {other, Proc1}
+        end
+    end,
     % return both names
+    [Other1, Other2].
+
+% register the process, wait for the {name, node} of the other side
+% then start loop
+damn_simple_link_startup(Spawner_process) ->
+    Self_name = register_unique(damn_simple_link, self()),
+    Spawner_process ! {name, {Self_name, node()}},
+    receive {other, Other_proc} -> 
+        damn_simple_link_loop(Other_proc, [])
+    end.
+
 damn_simple_link_loop(Other, Up_List) -> 
     receive
         % register the process whishing to receive notifications
-        {register, Pid} when is_pid(Pid) ->
+        {register, Pid} ->
             damn_simple_link_loop(Other, [Pid | Up_List]);
         % receive a message from the upper layer
         {send, Msg} ->
