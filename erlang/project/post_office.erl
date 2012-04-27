9e17cec0811d2a91327ab18316ebb58d0c718fbf
Martin Trigaux <me@mart-e.be>
2012-04-27 15:12:41 +0200

    add TODO

--- erlang/project/post_office.erl
+++ erlang/project/post_office.erl
@@ -4,11 +4,12 @@
 
 post_office(PO_List, Link) ->
     Start_loop = fun() ->
-        post_office_loop([], PO_List, [], Link, 0) 
+        % TODO replace by set instead of list
+        post_office_loop([], [], PO_List, Link, 0) 
     end,
     spawn(Start_loop).
 
-post_office_loop(My_Users, PO_List, All_Users, My_Link, PO_Seq) ->
+post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq) ->
     receive
         % add an internal new user
         {subscribe, User_Pid, Msg_Seq} ->
@@ -22,9 +23,10 @@ post_office_loop(My_Users, PO_List, All_Users, My_Link, PO_Seq) ->
                 end,
                 PO_List),
             post_office_loop(
+                % TODO replace by set instead of list
                 [User_Pid | My_Users],
-                PO_List,
                 dict:append(User_Pid, self(), All_Users),
+                PO_List,
                 My_Link,
                 PO_Seq+1);
 
@@ -37,10 +39,10 @@ post_office_loop(My_Users, PO_List, All_Users, My_Link, PO_Seq) ->
                     Dest_PO = dict:fetch(To_User, All_Users),
                     Enc_Msg = {send, From_User, To_User, Msg_Seq, M},
                     My_Link ! {transmit, self(), Dest_PO, PO_Seq, Enc_Msg},
-                    post_office_loop(My_Users, PO_List, All_Users, My_Link, PO_Seq+1);
+                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq+1);
                 true ->
                     io:format("Oups I don't know ~p~n", [To_User]),
-                    post_office_loop(My_Users, PO_List, All_Users, My_Link, PO_Seq)
+                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq)
             end;
             
         % a link deliver a message to the post office
@@ -52,20 +54,20 @@ post_office_loop(My_Users, PO_List, All_Users, My_Link, PO_Seq) ->
                     {From_User, To_User, Enc_Seq, Enc_Msg} = Body,
                     % TODO check To_User belongs to me
                     To_User ! {deliver, From_User, To_User, Enc_Seq, Enc_Msg},
-                    post_office_loop(My_Users, PO_List, All_Users, My_Link, PO_Seq);
+                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq);
                 % add an external user to the all users list
                 Action == subscribe ->
                     {User_Pid, PO_Pid, _} = Body,
                     post_office_loop(
                         My_Users,
-                        PO_List,
                         % add the user to the dic All_Users
                         dict:append(User_Pid, PO_Pid, All_Users),
+                        PO_List,
                         My_Link,
                         PO_Seq);
                 true ->
                     io:format("Unknown action ~p~n", [Action]),
-                    post_office_loop(My_Users, PO_List, All_Users, My_Link, PO_Seq)
+                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq)
             end
         
     end.
