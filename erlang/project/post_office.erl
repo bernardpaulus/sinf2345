bbe86471e2285ff10075e699e20956ab20b8d405
Martin Trigaux <me@mart-e.be>
2012-04-30 13:05:13 +0200

    remove seq number

--- erlang/project/post_office.erl
+++ erlang/project/post_office.erl
@@ -5,27 +5,25 @@
 post_office(PO_List, Link) ->
     Start_loop = fun() ->
         % create empty post office
-        post_office_loop(sets:new(), dict:new(), PO_List, Link, 0) 
+        post_office_loop(sets:new(), dict:new(), PO_List, Link) 
     end,
     spawn(Start_loop).
 
-post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq) ->
+post_office_loop(My_Users, All_Users, PO_List, My_Link) ->
     receive
         % add an internal new user
-        {subscribe, User_Pid, Msg_Seq} ->
+        {subscribe, User_Pid} ->
             % check avoid double subscription
             case sets:is_element(User_Pid, My_Users) of
                 true ->
                     io:format("The user ~p has already subscribed to me~n", [User_Pid]),
-                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq);
+                    post_office_loop(My_Users, All_Users, PO_List, My_Link);
                 false ->
                     list:foreach(
                         % to all post office, send a subscribe external user
                         fun(Post_Office) ->
-                            % encapsulated message, the Msg_Seq is NOT incremented for 
-                            % each post_office, just globally
-                            Enc_Msg = {subscribe, User_Pid, self(), Msg_Seq},
-                            My_Link ! {transmit, self(), Post_Office, PO_Seq, Enc_Msg}
+                            % encapsulated message
+                            My_Link ! {transmit, self(), Post_Office, {subscribe, User_Pid, self()}}
                         end,
                         PO_List),
                     post_office_loop(
@@ -33,40 +31,37 @@ post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq) ->
                         % Reflection : should we check the subscription of a user to 2 different post office ?
                         dict:append(User_Pid, self(), All_Users),
                         PO_List,
-                        My_Link,
-                        PO_Seq+1)
+                        My_Link)
             end;
 
 
         % a user send a message to another user
-        {send, From_User, To_User, Msg_Seq, M} ->
+        {send, From_User, To_User, M} ->
             Key = dict:find(To_User, All_Users),
             if
                 Key == ok ->
                     Dest_PO = dict:fetch(To_User, All_Users),
-                    Enc_Msg = {send, From_User, To_User, Msg_Seq, M},
-                    My_Link ! {send, self(), Dest_PO, PO_Seq, Enc_Msg},
-                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq+1);
+                    My_Link ! {send, self(), Dest_PO, {send, From_User, To_User, M}};
                 true ->
-                    io:format("Oups I don't know ~p~n", [To_User]),
-                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq)
-            end;
+                    io:format("Oups I don't know ~p~n", [To_User])
+            end,
+            post_office_loop(My_Users, All_Users, PO_List, My_Link);
             
         % a link deliver a message to the post office
-        {deliver, _From_PO, _To_PO, _Msg_Seq, M} ->
+        {deliver, _From_PO, _To_PO, M} ->
             {Action, Body} = M,
             if
                 % transmit the message to the corresponding user
                 Action == send ->
-                    {From_User, To_User, Enc_Seq, Enc_Msg} = Body,
+                    {From_User, To_User, Enc_Msg} = Body,
                     % check the user belongs to me
                     case sets:is_element(To_User, My_Users) of
                         true ->
-                            To_User ! {deliver, From_User, To_User, Enc_Seq, Enc_Msg};
+                            To_User ! {deliver, From_User, To_User, Enc_Msg};
                         false ->
                             io:format("The user ~p does not belong to me~n", [To_User])
                     end,
-                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq);
+                    post_office_loop(My_Users, All_Users, PO_List, My_Link);
 
                 % add an external user to the all users list
                 Action == subscribe ->
@@ -76,12 +71,11 @@ post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq) ->
                         % add the user to the dic All_Users
                         dict:append(User_Pid, PO_Pid, All_Users),
                         PO_List,
-                        My_Link,
-                        PO_Seq);
+                        My_Link);
 
                 true ->
                     io:format("Unknown action ~p~n", [Action]),
-                    post_office_loop(My_Users, All_Users, PO_List, My_Link, PO_Seq)
+                    post_office_loop(My_Users, All_Users, PO_List, My_Link)
             end
         
     end.
