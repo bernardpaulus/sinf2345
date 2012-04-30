202e4574fda1482938d99e6e7d6577176eb96c20
Bernard Paulus <bprecyclebin@gmail.com>
2012-04-30 13:38:16 +0200

    damn simple link & perfect link without seq num
    tested

--- erlang/project/link.erl
+++ erlang/project/link.erl
@@ -34,13 +34,11 @@ damn_simple_link(Nodes) when is_list(Nodes) ->
 % @type dsl_state() = #dsl_state{
 %    others = [pid()], 
 %    my_up = sets(), 
-%    all_up = dict(),
-%    seq = integer()}
+%    all_up = dict()}
 -record(dsl_state, {
     others = [], 
     my_up = sets:new(), 
-    all_up = dict:new(),
-    seq = 0}).
+    all_up = dict:new()}).
 
 % @spec (Nodes::[node()]) -> term()
 % @doc initializes the simple link and never returns.
@@ -49,42 +47,39 @@ damn_simple_link_init(Others) ->
 
 % @spec (dsl_state()) -> term()
 % @doc handle events.
-% {subscribe, Up, _Seq} -> register process in the set of destinations, <br/> 
-% the full mesh got a {subscribe, Up, _Seq}, <br/>
-% {send, _From, To, _Seq, _Msg} -> send a message to To, <br/>
-% {deliver, Other, Self, _Seq, M} -> transmit a message within the full mesh of
+% {subscribe, Up} -> register process in the set of destinations, <br/> 
+% the full mesh got a {subscribe, Up}, <br/>
+% {send, _From, To, _Msg} -> send a message to To, <br/>
+% {deliver, Other, Self, M} -> transmit a message within the full mesh of
 % damn_simple_link processes. Should not be used by other processes.
 damn_simple_link_loop(State) ->
     Self = self(),
     receive
-        {subscribe, Up, _} = M ->
-            #dsl_state{others=Others, seq=Seq_Num, my_up = My_Up, all_up=All_Up}
+        {subscribe, Up} = M ->
+            #dsl_state{others=Others, my_up = My_Up, all_up=All_Up}
                     = State,
             % transmit the request to all others
-            [Other ! {deliver, self(), Other, S, M} || 
-                    {Other, S} <- lists:zip(Others, lists:seq(Seq_Num, Seq_Num +
-                    length(Others) - 1)), Other /= self()],
+            [Other ! {deliver, self(), Other, M} || 
+                    Other <- Others, Other /= self()],
             damn_simple_link_loop(State#dsl_state{
                     my_up = sets:add_element(Up, My_Up),
-                    all_up = dict:append(Up, self(), All_Up), 
-                    seq = Seq_Num + length(Others)}); 
+                    all_up = dict:append(Up, self(), All_Up)}); 
 
-        {send, _, To, _, _}  = M ->
-            #dsl_state{seq = Seq_Num, all_up = All_Up} = State,
+        {send, _, To, _}  = M ->
+            #dsl_state{all_up = All_Up} = State,
             [Other] = dict:fetch(To, All_Up), % crash process if not found in dict
-            Other ! {deliver, self(), Other, Seq_Num, M},
-            damn_simple_link_loop(State#dsl_state{
-                    seq =  Seq_Num + 1});
+            Other ! {deliver, self(), Other, M},
+            damn_simple_link_loop(State);
 
-        {deliver, _, Self, _, {send, From, To, S, Msg}} -> 
+        {deliver, _, Self, {send, From, To, Msg}} -> 
             #dsl_state{my_up=My_Up} = State,
             % translate the "send" in "deliver"
             case sets:is_element(To, My_Up) of true ->
-                To ! {deliver, From, To, S, Msg},
+                To ! {deliver, From, To, Msg},
                 damn_simple_link_loop(State)
             end;
 
-        {deliver, Other, Self, _, {subscribe, Up, _}} -> 
+        {deliver, Other, Self, {subscribe, Up}} -> 
             All_Up = State#dsl_state.all_up,
             damn_simple_link_loop(State#dsl_state{
                     all_up = dict:append(Up, Other, All_Up)})
@@ -124,7 +119,6 @@ perfect_link([]) -> []. % particular case
     down = none, 
     my_up = sets:new(), 
     all_up = dict:new(),
-    seq = 1,
     buffer = [],
     delta = 100, % ms
     ttl = 64 % number of iterations
@@ -133,13 +127,13 @@ perfect_link([]) -> []. % particular case
 % @doc starts the perfect link.
 % this corresponds to the init event
 perfect_link_init(Others, Down) ->
-    Down ! {subscribe, self(), 0},
+    Down ! {subscribe, self()},
     perfect_link_loop(#pl_state{others = Others, down = Down}).
 
 % @doc loop while receiving events.
-% {subscribe, Up, Seq},
-% {send, From, To, Seq, Msg},
-% {deliver, From, To, Seq, Msg} -> used by the lower levels
+% {subscribe, Up},
+% {send, From, To, Msg},
+% {deliver, From, To, Msg} -> used by the lower levels
 perfect_link_loop(State0) ->
     % loop each 100 ms when messages are delayed/reordered, upon message reception
     % otherwise
@@ -150,45 +144,41 @@ perfect_link_loop(State0) ->
     State = perfect_link_loop_decrement_ttl(State0),
     Self = self(),
     receive
-        {subscribe, Up, _} = M ->
-            #pl_state{down = Down, others=Others, seq=Seq_Num, my_up = My_Up, 
+        {subscribe, Up} = M ->
+            #pl_state{down = Down, others=Others, my_up = My_Up, 
                     all_up=All_Up} = State,
             % transmit the request to all others
-            [Down ! {send, self(), Other, S, M} || 
-                    {Other, S} <- lists:zip(Others, lists:seq(Seq_Num, Seq_Num +
-                    length(Others) - 1)), Other /= self()],
+            [Down ! {send, self(), Other, M} || 
+                    Other <- Others, Other /= self()],
             perfect_link_loop(State#pl_state{
                     my_up = sets:add_element(Up, My_Up),
-                    all_up = dict:append(Up, self(), All_Up), 
-                    seq = Seq_Num + length(Others)}); 
+                    all_up = dict:append(Up, self(), All_Up)}); 
 
-        {send, _, To, _, _}  = M ->
-            #pl_state{down = Down, seq = Seq_Num, all_up = All_Up, ttl =
+        {send, _, To, _}  = M ->
+            #pl_state{down = Down, all_up = All_Up, ttl =
                 Max_TTL, buffer = Buffer} = State,
             [Other] = dict:fetch(To, All_Up), % crash process if not found in dict
-            Msg = {send, self(), Other, Seq_Num, M},
+            Msg = {send, self(), Other, M},
             P = random:uniform(),
             if 
                 P < 0.2 -> % Msg reordering
                     TTL = random:uniform(Max_TTL) + 1, % number of iterations
                     perfect_link_loop(State#pl_state{
-                            seq =  Seq_Num + 1,
                             buffer = [{TTL, Msg} | Buffer]});
                 true -> 
                     Down ! Msg,
-                    perfect_link_loop(State#pl_state{
-                            seq =  Seq_Num + 1})
+                    perfect_link_loop(State)
             end;
 
-        {deliver, _, Self, _, {send, From, To, S, Msg}} -> 
+        {deliver, _, Self, {send, From, To, Msg}} -> 
             #pl_state{my_up = My_Up} = State,
             % translate the "send" in "deliver"
             case sets:is_element(To, My_Up) of true ->
-                To ! {deliver, From, To, S, Msg},
+                To ! {deliver, From, To, Msg},
                 perfect_link_loop(State)
             end;
 
-        {deliver, Other, Self, _, {subscribe, Up, _}} -> 
+        {deliver, Other, Self, {subscribe, Up}} -> 
             All_Up = State#pl_state.all_up,
             perfect_link_loop(State#pl_state{
                     all_up = dict:append(Up, Other, All_Up)})
