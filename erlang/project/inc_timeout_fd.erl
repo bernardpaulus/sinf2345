dbe15ef5959258c3f405bbe8a4dd1d73b9cea4fa
Bernard Paulus <bprecyclebin@gmail.com>
2012-05-02 10:11:54 +0200

    incremental_timeout faileure detector debugged :)

--- erlang/project/inc_timeout_fd.erl
+++ erlang/project/inc_timeout_fd.erl
@@ -19,19 +19,20 @@ start([]) -> [].
 
 -record(it_state, {
     down = none,
-    others = [],
+    others = no_other_one,
     alive = sets:new(),
     suspected = sets:new(),
     delay = ?delta,
     my_up = sets:new(),
-    all_up = dict:new()
+    ups_of_others = dict:new()
     }).
 
 init(Others, Down) ->
     Down ! {subscribe, self()},
-    erlang:send_after(?delta, self(), {timeout}),
+    erlang:send_after(?delta, self(), {timeout, self()}),
     inc_timeout_loop(#it_state{
         down = Down,
+        others = Others,
         alive = sets:from_list(Others)}).
 
 
@@ -40,58 +41,35 @@ inc_timeout_loop(State) ->
     receive
         {subscribe, Up} = M ->
             #it_state{down = Down, others=Others, my_up = My_Up, 
-                    all_up=All_Up} = State,
+                    ups_of_others=O_Ups} = State,
             % transmit the request to all others
             [Down ! {send, self(), Other, M} || 
                     Other <- Others, Other /= self()],
             inc_timeout_loop(State#it_state{
                     my_up = sets:add_element(Up, My_Up),
-                    all_up = dict:append(Up, self(), All_Up)}); 
+                    ups_of_others = dict:append(self(), Up, O_Ups)}); 
 
 
         {deliver, Other, Self, {subscribe, Up}} -> 
-            All_Up = State#it_state.all_up,
+            O_Ups = State#it_state.ups_of_others,
             inc_timeout_loop(State#it_state{
-                    all_up = dict:append(Up, Other, All_Up)});
+                    ups_of_others = dict:append(Other, Up, O_Ups)});
 
 
-        {timeout} ->
+        {timeout, Self} ->
             #it_state{alive = Alive, suspected = Suspected, delay = Delay,
                     others = Others} = State,
+            %io:format("~p delay: ~p~n", [Self, Delay]),
             case sets:size(sets:intersection(Alive, Suspected)) of
                 X when X > 0 ->
                     State1 = State#it_state{delay = Delay + ?delta};
                 0 -> 
                     State1 = State
             end,
-            State2 = lists:foldl(fun (P, State_Acc) ->
-                    #it_state{suspected = Suspect, down = Down,
-                        my_up = My_Ups, all_up = All_Up} = State_Acc,
-                    Is_Alive = sets:is_element(P, Alive),
-                    Is_Suspect = sets:is_element(P, Suspect),
-                    Ups_P = sets:from_list(
-                        case dict:find(P, All_Up) of
-                            {ok, Values} -> Values;
-                            error -> []
-                        end),
-                    if 
-                        (not Is_Alive) and Is_Suspect ->
-                            NewS = sets:add_element(P, Suspect),
-                            [Up ! {suspect, P, Ups_P} || 
-                                    Up <- sets:to_list(My_Ups)];
-                        Is_Alive and Is_Suspect ->
-                            NewS = sets:del_element(P, Suspect),
-                            [Up ! {restore, P, Ups_P} || 
-                                    Up <- sets:to_list(My_Ups)];
-                        true ->
-                            NewS = Suspect
-                    end,
-                    Down ! {send, Self, P, heartbeat_request},
-                    State_Acc#it_state{suspected = NewS}
-                end,
-                State1, Others),
+            State2 = lists:foldl(fun suspect_restore/2, State1, 
+                    [O || O <- Others, O /= self()]),
             State3 = State2#it_state{alive = sets:new()}, % empty alive
-            erlang:send_after(State3#it_state.delay, Self, {timeout}),
+            erlang:send_after(State3#it_state.delay, Self, {timeout, Self}),
             inc_timeout_loop(State3);
 
 
@@ -106,3 +84,34 @@ inc_timeout_loop(State) ->
             inc_timeout_loop(State#it_state{
                 alive = sets:add_element(From, Alive)})
     end.
+
+suspect_restore(P, State_Acc) ->
+    Self = self(),
+    #it_state{suspected = Suspect, down = Down, alive = Alive,
+        my_up = My_Ups, ups_of_others = O_Ups} = State_Acc,
+    Is_Alive = sets:is_element(P, Alive),
+    Is_Suspect = sets:is_element(P, Suspect),
+    Ups_P = sets:from_list(
+        case dict:find(P, O_Ups) of
+            {ok, Values} -> Values;
+            error -> []
+        end),
+    if 
+        (not Is_Alive) and (not Is_Suspect) ->
+            NewS = sets:add_element(P, Suspect),
+            [Up ! {suspect, P, Ups_P} || 
+                    Up <- sets:to_list(My_Ups)];
+        Is_Alive and Is_Suspect ->
+            NewS = sets:del_element(P, Suspect),
+            [Up ! {restore, P, Ups_P} || 
+                    Up <- sets:to_list(My_Ups)];
+        true ->
+            NewS = Suspect
+    end,
+    Down ! {send, Self, P, heartbeat_request},
+    State_Acc#it_state{suspected = NewS}.
+
+foldl(Fun, Acc, [H | T]) ->
+    foldl(Fun, Fun(H, Acc), T);
+    foldl(_, Acc, []) -> Acc.
+
