25f09c12ca57d38f12618dc1344b7b73f9fe1902
Martin Trigaux <martvefun@gmail.com>
2012-05-03 19:13:11 +0200

    cleaning

--- erlang/project/ld_cons.erl
+++ erlang/project/ld_cons.erl
@@ -15,24 +15,25 @@
           newts = 0,
           newl = none,
           epoch_cons = none,
-          epoch_chang = none,
-          beb = none,
-          link = none
+          epoch_chang = none
           }).
 
 %% Epoch_Chang : a Monarchical Eventual Leader Detector
 %% Peers : peers in the monarch_eld
-%% Link : a p2p link
-%% Be : a Best effort broadcast
-init(Epoch_Chang, Peers, Link, Beb) ->
-    %% Bebs = beb:start([Node()]),
-    %% Links = link:damn_simple_link([Node()]),
-    L0 = monarch_eld:max_rank(Peers, Peers),
+init(Epoch_Cons, Epoch_Chang, Peers) ->
+    L0 = monarch_eld:max_rank(Peers, sets:from_list(Peers)),
+    Epoch_Cons ! {suscribe, self()},
+    Epoch_Chang ! {suscribe, self()},
     %% create initial EpochConsensus
-    Epoch_Cons = rw_epoch_cons:start(Beb, Link, 0, L0),
-    ldc_loop(#ldc_state{val = none, proposed = false,
-                        decided = false, epoch_cons = Epoch_Cons,
-                        epoch_chang = Epoch_Chang, beb = Beb}).
+    ldc_loop(#ldc_state{val = none,
+                        proposed = false,
+                        decided = false,
+                        epoch_cons = Epoch_Cons,
+                        epoch_chang = Epoch_Chang,
+                        ets = 0,
+                        lead = L0,
+                        newts = 0,
+                        newl = none}).
 
 ldc_loop(State) ->
     Self= self(),
@@ -49,11 +50,11 @@ ldc_loop(State) ->
         
         %% The EpochConsensus has been aborted
         {aborted, Abo_State, Abo_TS} when State#ldc_state.ets == Abo_TS ->
-            #ldc_state{newts = New_TS, newl = New_L} = State,
+            #ldc_state{epoch_cons = EC, newts = New_TS, newl = New_L} = State,
             %% initialize a new instance of epoch consensus with timestamp ets
             %% Epoch_Cons = rw_epoch_cons:start(Beb, Link, New_TS, Abo_State),
-            Epoch_Cons = super:reinit(New_TS, Abo_State),
-            ldc_loop(State#ldc_state{proposed = false, ets = New_TS, newl = New_L, epoch_cons = Epoch_Cons});
+            Epoch_Cons = rw_epoch_cons:reinit(EC, New_TS, Abo_State),
+            ldc_loop(State#ldc_state{proposed = false, ets = New_TS, lead = New_L, epoch_cons = Epoch_Cons});
 
         %% Decide on a value
         {decide, Val, Ets} when State#ldc_state.ets == Ets ->
