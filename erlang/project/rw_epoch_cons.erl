103f62a637c6653410d780a639639061b48a3696
Bernard Paulus <bprecyclebin@gmail.com>
2012-05-03 18:43:45 +0200

    dynamic peer discovery (too complex !!!)

--- erlang/project/rw_epoch_cons.erl
+++ erlang/project/rw_epoch_cons.erl
@@ -4,7 +4,7 @@
 -module(rw_epoch_cons).
 -compile(export_all).
 
--import(spawn_utils, [spawn_multiple_on_top/2]).
+-import(spawn_utils, [spawn_multiple_on_top/3]).
 
 -record(rwe_state, {
         leader = none,
@@ -20,38 +20,187 @@
         my_ups = []
         }).
 
-% spawn a single process TODO spec: cf start() below
-%start(Beb, Link, Epoch_Ts, State) ->
 
-% @spec (Bebs, Links, Epoch_Ts, States) -> Rw_epoch_cons :: [pid()]
+% start stuff
+-define(base_start_delay, 100).
+-define(max_start_delay, 1600).
+
+-record(startrw_state, {
+        beb = beb,
+        link = link,
+        ets = ets,
+        epoch_state = e_state,
+        propose = none,
+        leader = none,
+        start_delay = ?base_start_delay,
+        replies = sets:new(),
+        n = -1
+        }).
+
+% @spec (Bebs, Links, Epoch_Ts, E_States) -> Rw_epoch_cons :: [pid()]
 %   Bebs = [Beb :: pid()]
 %   Links = [Link :: pid()]
 %   Epoch_Ts = [integer()]
-%   States = [{Ts :: integer(), Val :: term()}]
+%   E_States = [{Ts :: integer(), Val :: term()}]
+%   N = integer()
 % @doc spawns a rw_consensus_epoch instance with the given Epoch_Ts.
-start(Bebs, Links, Epoch_Ts, States) when 
+start(Bebs, Links, Epoch_Ts, E_States, N) when 
         is_pid(hd(Bebs)), is_pid(hd(Links)), 
-        length(Bebs) == length(Links), length(Links) == length(States) ->
-    spawn_multiple_on_top(Bebs, Epoch_Ts).
+        length(Bebs) == length(Links), length(Links) == length(E_States) ->
+    spawn_multiple_on_top(Bebs, [fun init/6 || _ <- Bebs], 
+            [[Link, Epoch_Ts, E_State, N] || {Link, E_State} 
+                <- lists:zip(Links, E_States)]);
+    
+% @spec (Beb, Link, Epoch_Ts, E_State, N) -> pid()
+%   Beb = pid()
+%   Link = pid()
+%   Epoch_Ts = integer()
+%   E_State = {Ts :: integer(), Val :: term()}
+%   N = integer()
+% @doc spawn a read write epoch consensus process, and discovers peers from
+% the same read write epoch consensus. Non-leader nodes wait on the leader to
+% enter the core algorithm. Leader waits for a {propose, V}, and then start
+% broadcast and wait for ack rounds to ensure enough nodes are started and
+% participate in the algorithm.
+start(Beb, Link, Epoch_Ts, E_State, N) ->
+    start(#startrw_state{
+        beb = Beb,
+        link = Link,
+        ets = Epoch_Ts,
+        epoch_state = E_State,
+        n = N}).
+
+% @doc waits for a {propose} to know whether self is the leader.
+start(State) ->
+    Self = self(),
+    #startrw_state{ets = Ets} = State,
+    receive
+        {propose, _} = M ->
+            #startrw_state{beb = Beb} = State,
+            Beb ! {broadcast, Self, {start, Ets}},
+            % wait for answers in start_leader()
+            erlang:send_after(?base_start_delay, Self, {start_timeout}),
+            start_leader(State#startrw_state{
+                propose = M,
+                leader = Self});
+
+        {deliver, Leader, {start, Ets}} ->
+            #startrw_state{link = Link} = State,
+            Link ! {send, Self, Leader, {ack_start, Ets}},
+            start_normal(State#startrw_state{
+                leader = Leader});
+
+        {abort, Pid, Ets} ->
+            Pid ! {aborted, State#startrw_state.epoch_state, Ets}
+
+    end.
+
+
+start_leader(State) ->
+    #startrw_state{start_delay = T, n = N} = State,
+    % wait and double timer
+    receive {start_timeout} -> Timeout = T * 2 end,
+    State1 = leader_empty_buffer(State#startrw_state{
+                start_delay = Timeout}),
+    #startrw_state{replies = Replies} = State1,
+    N_Replies = sets:size(Replies),
+    if 
+        N_Replies > N / 2 ->
+            % ok, let's go!
+            #startrw_state{beb = Beb, link = Link, ets = Epoch_Ts, 
+                epoch_state = E_State, n = N, propose = Propose} = State1,
+            % re-send received propose
+            self() ! Propose,
+            init([], Beb, Link, Epoch_Ts, E_State, N);
+
+        Timeout =< ?max_start_delay ->
+            erlang:send_after(Timeout, self(), {start_timeout}),
+            start_leader(State1#startrw_state{
+                replies = sets:new()});
+
+        true ->
+            throw("start_leader: not enough replies in time")
+    end.
+
+
+leader_empty_buffer(State) ->
+    #startrw_state{ets = Ets} = State,
+    Leader = Self = self(),
+    receive
+        {propose, _} -> 
+            throw("duplicated propose to leader");
+
+        {deliver, Leader, {start, Ets}} ->
+            #startrw_state{link = Link} = State,
+            Link ! {send, Self, Leader, {ack_start, Ets}},
+            leader_empty_buffer(State);
+            
+        {deliver, _ , {start, Ets}} ->
+            throw("two leaders... die");
+
+        {deliver, Pid, Self, {ack_start, Ets}} ->
+            leader_empty_buffer(State#startrw_state{
+                replies = sets:add_element(Pid, State#startrw_state.replies)});
+
+        {abort, Pid, Ets} ->
+            Pid ! {aborted, State#startrw_state.epoch_state, Ets}
+
+    after 0 ->
+        State
+    end.
+
+start_normal(State) ->
+    #startrw_state{ets = Ets, leader = L} = State,
+    Self = self(),
+    receive
+        {propose, _} -> 
+            throw("another process is leader");
+
+        {deliver, Leader, {start, Ets}} when L == none ->
+            #startrw_state{link = Link} = State,
+            Link ! {send, Self, Leader, {ack_start, Ets}},
+            start_normal(State#startrw_state{
+                    leader = Leader});
+
+        {deliver, L, {start, Ets}} ->
+            #startrw_state{link = Link} = State,
+            Link ! {send, Self, L, {ack_start, Ets}},
+            start_normal(State);
+
+        {deliver, _Other, {start, Ets}} -> % not none, nor the usual leader
+            throw("two or more leaders in rw_epoch_cons");
+
+        {deliver, L, {read, Ets}} = M ->
+            % start of the protocol
+            #startrw_state{beb = Beb, link = Link, ets = Epoch_Ts, 
+                epoch_state = E_State, n = N} = State,
+            % resend it to the core algo
+            Self ! M,
+            init([], Beb, Link, Epoch_Ts, E_State, N);
+            
+        {abort, Pid, Ets} ->
+            Pid ! {aborted, State#startrw_state.epoch_state, Ets}
+    end.
     
 
-% @spec (Peers, Beb, Link, Epoch_Ts, State) -> void
-%   Peers = [pid()]
+
+% @spec (_Peers, Beb, Link, Epoch_Ts, E_State) -> void
+%   _Peers = [pid()]
 %   Beb = pid()
 %   Link = pid()
 %   Epoch_Ts = integer()
 %   Self_Leader = boolean()
-init(Peers, Beb, Link, Epoch_Ts, State) ->
+%   N = integer()
+init(_Peers, Beb, Link, Epoch_Ts, E_State, N) ->
     Link ! {subscribe, self()},
     Beb ! {subscribe, self()},
     % conditions on state (must call check to test & trigger them)
     condition:start(),
-    N = length(Peers),
     Self = self(),
     condition:upon(
         % #states > N/2
-        fun (#rwe_state{states = States}) ->
-            dict:size(States) > N / 2
+        fun (#rwe_state{states = E_States}) ->
+            dict:size(E_States) > N / 2
         end,
         % action
         fun (_) ->
@@ -66,9 +215,8 @@ init(Peers, Beb, Link, Epoch_Ts, State) ->
             Self ! {accepted_over_N_div_2}
         end),
         
-    {Val_Ts, Val} = State,
+    {Val_Ts, Val} = E_State,
     loop(#rwe_state{
-        peers = Peers,
         beb = Beb,
         link = Link,
         ets = Epoch_Ts,
