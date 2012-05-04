e1b0c63be7352fb7eca7fc68b4f6e3ec56ef4e1b
Martin Trigaux <martvefun@gmail.com>
2012-05-04 16:36:48 +0200

    better tests

--- erlang/project/test.erl
+++ erlang/project/test.erl
@@ -4,19 +4,31 @@
 ldc() ->
     %% The links
     Links = link:perfect_link([node(), node(), node()]),
+    receive after 100 -> pass end,
     %% The failure detectors
     Fail_Dets = inc_timeout_fd:start(Links),
+    receive after 100 -> pass end,
     %% Down, leader detectors
     Downs = monarch_eld:start(Fail_Dets, Links),
+    receive after 100 -> pass end,
     %% Best effort broadcasts
     Bebs = beb:start(Links),
-
+    receive after 100 -> pass end,
     %% The epoch change
     Epoch_Changes = epoch_change:start(Downs, Bebs, Links),
+    receive after 100 -> pass end,
     %% The initial states of the epoch consensus
     E_States = [{0, bottom} || _ <- Bebs],
     %% The epoch consensus
-    Epoch_Conss = rw_epoch_cons:start(Bebs, Links, 0, E_States),
-    
+    Epoch_Conss = [E1, _E2, _E3] = rw_epoch_cons:start(Bebs, Links, 0, E_States),
+    receive after 100 -> pass end,
     %% The ldc object
-    ld_cons:start(Epoch_Conss, Epoch_Changes).
+    _LDCs = [A, _B, _C] = ld_cons:start(Epoch_Conss, Epoch_Changes),
+
+    %% Debug mode
+    dbg:tracer(),
+    dbg:p(A,m),
+    dbg:p(E1,m),
+
+    receive after 100 -> pass end,
+    A ! {propose, myval}.
