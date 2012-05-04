839210916f6a919bb033258ed422915ec03b16e0
Martin Trigaux <me@mart-e.be>
2012-05-05 00:36:58 +0200

    refactoring messages

--- erlang/project/bank.erl
+++ erlang/project/bank.erl
@@ -1,26 +1,41 @@
 -module(bank).
+-import(spawn_utils, [spawn_multiple_on_top/2]).
+-record(bank_state, {
+          tob = none,
+          accounts = dict:new()}).
 
--record(bank_state, {}).
+start(TOBs)
+  when is_pid(hd(TOBs)) ->
+    spawn_multiple_on_top(TOBs, [fun init/2 || _ <- TOBs]).
 
 start() ->
 	Nodes = [node(), node(), node()],
 	TOBs = tob:start(Nodes),
+    start(TOBs).
+
     
-	loop(#bank_state{}).
+init(_Peers, TOB) ->
+	loop(#bank_state{tob = TOB}).
+
 
 loop(State) ->
 	receive
-        %% transfer from an account to another
-        {transfer, From, To, Amount} ->
-            pass;
+        %% create a new account
+        {create, Account_ID, Amount} ->
+            #bank_state{accounts = Accounts} = State,
+            New_Acc = dict:append(Account_ID, Amount, Accounts),
+            loop(State#bank_state{accounts = New_Acc});
         
-        %% check the amount left on the account
-        {check, From} ->
-            pass;
+        %% update the amount of an account to a certain value
+        {transfer, Account, Amount} ->
+            #bank_state{accounts = Accounts} = State,
+            Total = dict:fetch(Account, Accounts),
+            New_Acc = dict:update(Account,
+                                  fun(Amount) -> Total + Amount end,
+                                  Accounts),
+            
+            loop(State#bank_state{accounts = New_Acc})
         
-        %% recieve confirmation of a transaction
-        {ack, From, M} ->
-            pass
     end.
 
 	
