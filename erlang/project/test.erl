7aa36573fe677aedb4f8d1d55b98e76ba1217aa0
Bernard Paulus <bprecyclebin@gmail.com>
2012-05-06 16:38:04 +0200

    fixed typo

--- erlang/project/test.erl
+++ erlang/project/test.erl
@@ -20,15 +20,15 @@ ldc() ->
     %% The initial states of the epoch consensus
     E_States = [{0, bottom} || _ <- Bebs],
     %% The epoch consensus
-    Epoch_Conss = [E1, _E2, _E3] = rw_epoch_cons:start(Bebs, Links, 0, E_States),
+    Epoch_Conss = [_E1, _E2, _E3] = rw_epoch_cons:start(Bebs, Links, 0, E_States),
     receive after 100 -> pass end,
     %% The ldc object
     _LDCs = [A, _B, _C] = ld_cons:start(Epoch_Conss, Epoch_Changes),
 
     %% Debug mode
-    dbg:tracer(),
-    dbg:p(A,m),
-    dbg:p(E1,m),
+    %dbg:tracer(),
+    %dbg:p(A,m),
+    %dbg:p(E1,m),
 
     receive after 100 -> pass end,
     A ! {propose, myval}.
